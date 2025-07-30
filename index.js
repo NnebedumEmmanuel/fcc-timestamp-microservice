9deaa5596a63179badbdc4d382691e903fcd73fb
NnebedumEmmanuel <emmanuelnnebedum704@gmail.com>
2025-07-30 13:07:20 +0100

    update

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Timestamp Microservice is working!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
